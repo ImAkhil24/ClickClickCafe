08911ecd9a9f819027ff29413e834a84999ddbed
ImAkhil24 <akhilbadoni24@gmail.com>
2021-08-03 12:51:38 +0530

    header finished

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,12 +1,13 @@
 import {Fragment} from 'react';
 import classes from "./Header.module.css";
 import mealImage from "../../assets/meals.jpg";
+import HeaderCartButton from './HeaderCartButton';
 
 const Header = props => {
   return <Fragment>
     <header className={classes.header}>
       <h1>Click Click Cafe</h1>
-      <button>Cart</button>
+      <HeaderCartButton/>
     </header>
     <div className={classes['main-image']}>
       <img src = {mealImage} alt="some super cool thing"/>
