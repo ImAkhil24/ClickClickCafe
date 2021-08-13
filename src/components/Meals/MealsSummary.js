f50b0dc9011b37dbb77aea4d1bf958f11a745177
ImAkhil24 <akhilbadoni24@gmail.com>
2021-08-13 22:05:36 +0530

    few changes

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,18 +1,17 @@
 import classes from "./MealsSummary.module.css";
 
-const MealsSummary = props => {
-  return(
+const MealsSummary = (props) => {
+  return (
     <section className={classes.summary}>
-      <h2>
-        Your Favourite Homemade food.
-      </h2>
+      <h2>Your Favourite food.</h2>
 
       <p>
         Choose your favourite meal from out broad section of available meals.
       </p>
 
       <p>
-        All our meals are cooked with high quality ingredients, just in time of course by experienced chefs and with full Covid19 measures.
+        All our meals are cooked with high quality ingredients, just in time of
+        course by experienced chefs and with full Covid19 measures.
       </p>
     </section>
   );
